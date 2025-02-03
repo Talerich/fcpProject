0fb108fa7af9c702daa751bd0bb630af27b43445
erfanRmoghadam <Erfanramezani47@gmail.com>
2025-02-02 17:45:12 -0800

    added struct.student

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <vector>
 using namespace std;
+struct student
+{
+    int code;
+    string name;
+    string major;
+    vector<string> subjects;
+    vector<int> scores;
+};
 int main()
 {
     return 0;
