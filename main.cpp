d6bf928b18388ed461182a11c35173215b481b7c
erfanRmoghadam <Erfanramezani47@gmail.com>
2025-02-02 17:50:41 -0800

    added cases

--- main.cpp
+++ main.cpp
@@ -18,7 +18,13 @@ int main()
         cout << "1-Add a student\n2-Add Info about student\n3-Edit a student Info\n4-Show students list\n5-Show academic record\n6-exit\nChoose your option : ";
         cin >> task;
         switch(task){
-            
+        case 1 : break;
+        case 2 : break;
+        case 3 : break;
+        case 4 : break;
+        case 5 : break;
+        case 6 : break;
+
         }
     } while (task <=6);
     return 0;
